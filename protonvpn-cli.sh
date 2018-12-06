9072bab0cc17c2532126cebc7ec9d18a70ecc09a
Alex Manuskin <amanusk@protonmail.com>
2018-12-06 12:30:46 +0200

    Do not require root for version (#157)

--- protonvpn-cli.sh
+++ protonvpn-cli.sh
@@ -12,7 +12,8 @@ version=1.1.2
 if [[ ("$UID" != 0) && ("$1" != "ip") && ("$1" != "-ip") && \
       ("$1" != "--ip") && ! (-z "$1") && ("$1" != "-h") && \
       ("$1" != "--help") && ("$1" != "--h") && ("$1" != "-help") && \
-      ("$1" != "help") ]]; then
+      ("$1" != "help") && ("$1" != "--version") && ("$1" != "-version") && \
+      ("$1" != "-v") && ("$1" != "--v")]]; then
   echo "[!] Error: The program requires root access."
   exit 1
 fi
